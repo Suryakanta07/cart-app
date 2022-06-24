e8bde547eb0dec604dc7d360fe89dfbcef8aeb28
Suryakanta07 <sbehera807@gmail.com>
2022-06-24 13:41:35 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import pic1 from './img/pic1.jpg';
+import pic2 from './img/pic2.jpg';
+import pic3 from './img/pic3.jpg';
+
+const data = {
+    productData:[
+        {
+          id: 1,
+          img: pic1,
+          title: "Cotton Shirt",
+          dec: '',
+          price: 330,
+        },
+        {
+            id: 2,
+            img: pic2,
+            title: "Full Sleeve Shirt",
+            dec: '',
+            price: 570,
+          },
+          {
+            id: 3,
+            img: pic3,
+            title: "Black Shirt",
+            dec: '',
+            price: 420,
+          }
+    ],
+};
+
+export default data;
